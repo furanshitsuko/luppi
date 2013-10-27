34592f9e03d1dbd7c0e544c4e29ac3cf93477979
juan fuenzalida <furanshitsuko@outlook.com>
2013-10-27 00:59:00 -0300

    Create psu.cpp
    programa c++

---
+++ psu.cpp
@@ -0,0 +1,251 @@
+#include<stdio.h>
+#include <stdlib.h> 
+main(){
+
+float variable;
+float nem;
+float ranking;
+float lyc;
+float mat;
+float ciencias;
+float pcorte;
+
+printf("-1. Ing comercial \n-2.ing civil industrial\n-3.terminar");
+scanf("%f",&variable);
+
+if(variable=1){
+system ("cls");
+printf("ingrese su puntaje: \n");
+printf("nem: \n");
+scanf("%f",nem);
+printf("ranking: \n");
+scanf("%f",&ranking);
+printf("lyc: \n");
+scanf("%f",&lyc);
+printf("mat: \n");
+scanf("%f",&mat);
+printf("ciencias: \n");
+scanf("%f",&ciencias);
+
+float nemcon;
+float rankingcon;
+float lyccon;
+float matcon;
+float cienciascon;
+
+nemcon=nem*0.15;
+rankingcon=ranking*0.25;
+lyccon=lyc*0.15;
+matcon=mat*0.35;
+cienciascon=ciencias*0.10;
+pcorte=nemcon+rankingcon+lyccon+matcon+cienciascon;
+if(pcorte=683.5){
+printf("puede ingresar a la universidad de concepcion");
+}
+else{
+printf("no puede ingresar a la universidad de concepcion");
+}
+
+float nemchi;
+float rankingchi;
+float lycchi;
+float matchi;
+float cienciaschi;
+float pcortechi;
+
+nemchi=nem*0.10;
+rankingchi=ranking*0.20;
+lycchi=lyc*0.10;
+matchi=mat*0.45;
+cienciaschi=ciencias*0.15;
+pcortechi=nemchi+rankingchi+lycchi+matchi+cienciaschi;
+
+if(pcortechi=718.3){
+printf("puede ingresar a la universidad de chile");
+}else{
+printf("no puede ingresar a la universidad de chile");
+}
+
+float nemcat;
+float rankingcat;
+float lyccat;
+float matcat;
+float cienciascat;
+float pcortecat;
+
+nemcat=nem*0.20;
+rankingcat=ranking*0.20;
+lyccat=lyc*0.10;
+matcat=mat*0.35;
+cienciascat=ciencias*0.15;
+pcortecat=nemcat+rankingcat+lyccat+matcat+cienciascat;
+
+if(pcortecat=727.3){
+printf("puede ingresar a la universidad catolica");
+}else{
+printf("no puede ingresar a la universidad catolica");
+}
+
+float nemdiego;
+float rankingdiego;
+float lycdiego;
+float matdiego;
+float cienciasdiego;
+float pcortediego;
+
+nemdiego=nem*0.10;
+rankingdiego=ranking*0.10;
+lycdiego=lyc*0.10;
+matdiego=mat*0.60;
+cienciasdiego=ciencias*0.10;
+pcortediego=nemdiego+rankingdiego+lycdiego+matdiego+cienciasdiego;
+
+if(pcortediego=604.7){
+printf("puede ingresar a la universidad diego portales");
+}else{
+printf("no puede ingresar a la universidad diego portales");
+}
+
+float nemadolfo;
+float rankingadolfo;
+float lycadolfo;
+float matadolfo;
+float cienciasadolfo;
+float pcorteadolfo;
+
+nemadolfo=nem*0.10;
+rankingadolfo=ranking*0.10;
+lycadolfo=lyc*0.10;
+matadolfo=mat*0.60;
+cienciasadolfo=ciencias*0.10;
+pcorteadolfo=nemadolfo+rankingadolfo+lycadolfo+matadolfo+cienciasadolfo;
+
+if(pcorteadolfo=604.2){
+printf("puede ingresar a la universidad adolfo ibañez");
+}else{
+printf("no puede ingresar a la universidad adolfo ibañez");
+}
+}
+
+
+if(variable=2){
+system ("cls");
+printf("ingrese su puntaje: \n");
+printf("nem: \n");
+scanf("%f",nem);
+printf("ranking: \n");
+scanf("%f",&ranking);
+printf("lyc: \n");
+scanf("%f",&lyc);
+printf("mat: \n");
+scanf("%f",&mat);
+printf("ciencias: \n");
+scanf("%f",&ciencias);
+
+float nemcon;
+float rankingcon;
+float lyccon;
+float matcon;
+float cienciascon;
+
+nemcon=nem*0.15;
+rankingcon=ranking*0.25;
+lyccon=lyc*0.15;
+matcon=mat*0.35;
+cienciascon=ciencias*0.10;
+pcorte=nemcon+rankingcon+lyccon+matcon+cienciascon;
+if(pcorte=587.9){
+printf("puede ingresar a la universidad de concepcion");
+}else{
+printf("no puede ingresar a la universidad de concepcion");
+}
+
+float nemchi;
+float rankingchi;
+float lycchi;
+float matchi;
+float cienciaschi;
+float pcortechi;
+
+nemchi=nem*0.10;
+rankingchi=ranking*0.20;
+lycchi=lyc*0.10;
+matchi=mat*0.45;
+cienciaschi=ciencias*0.15;
+pcortechi=nemchi+rankingchi+lycchi+matchi+cienciaschi;
+
+if(pcortechi=708.1){
+printf("puede ingresar a la universidad de chile");
+}else{
+printf("no puede ingresar a la universidad de chile");
+}
+
+float nemcat;
+float rankingcat;
+float lyccat;
+float matcat;
+float cienciascat;
+float pcortecat;
+
+nemcat=nem*0.20;
+rankingcat=ranking*0.20;
+lyccat=lyc*0.10;
+matcat=mat*0.35;
+cienciascat=ciencias*0.15;
+pcortecat=nemcat+rankingcat+lyccat+matcat+cienciascat;
+
+if(pcortecat=728.5){
+printf("puede ingresar a la universidad catolica");
+}else{
+printf("no puede ingresar a la universidad catolica");
+}
+
+float nemdiego;
+float rankingdiego;
+float lycdiego;
+float matdiego;
+float cienciasdiego;
+float pcortediego;
+
+nemdiego=nem*0.10;
+rankingdiego=ranking*0.10;
+lycdiego=lyc*0.10;
+matdiego=mat*0.60;
+cienciasdiego=ciencias*0.10;
+pcortediego=nemdiego+rankingdiego+lycdiego+matdiego+cienciasdiego;
+
+if(pcortediego=600){
+printf("puede ingresar a la universidad diego portales");
+}else{
+printf("no puede ingresar a la universidad diego portales");
+}
+
+float nemadolfo;
+float rankingadolfo;
+float lycadolfo;
+float matadolfo;
+float cienciasadolfo;
+float pcorteadolfo;
+
+nemadolfo=nem*0.10;
+rankingadolfo=ranking*0.10;
+lycadolfo=lyc*0.10;
+matadolfo=mat*0.60;
+cienciasadolfo=ciencias*0.10;
+pcorteadolfo=nemadolfo+rankingadolfo+lycadolfo+matadolfo+cienciasadolfo;
+
+if(pcorteadolfo=600){
+printf("puede ingresar a la universidad adolfo ibañez");
+}else{
+printf("no puede ingresar a la universidad adolfo ibañez");
+}
+}
+
+if(variable=3){
+system ("cls");
+return 0;
+}
+
+}
+
+
